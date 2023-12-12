514e0ddfa7e25f0c4806679f83704cc7f5782e92
Brett Connell <dev@bretta.io>
2023-12-12 08:59:37 -0500

    having a crack at the google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,19 +5,14 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <script>
-                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-KPK8837Q');`}
-                </script>
                 {/* Favicon  */}
                 <link rel="icon" type="icon" href="/assets/images/favicon.ico" />
                 {/* Fonts  */}
                 <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;700;800&display=swap" rel="stylesheet" />
             </Head>
             <body>
+                {`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPK8837Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`}
                 <Main />
                 <NextScript />
             </body>
